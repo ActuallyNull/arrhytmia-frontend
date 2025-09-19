f25dfeecc587b4752693d2a868f1e4ec66576750
null <thenewgamerpro76@gmail.com>
2025-09-18 21:10:01 -0400

    tried something else

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,24 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 3000,
-    proxy: {
-      '/api': {
-        target: 'http://localhost:8000',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, '')
+export default defineConfig(({ mode }) => {
+  // Explicitly load env vars
+  const env = loadEnv(mode, process.cwd(), '')
+
+  console.log('=== Vite build environment variables ===')
+  console.log(env) // should include your VITE_* keys if Render is injecting them
+
+  return {
+    plugins: [react()],
+    server: {
+      port: 3000,
+      proxy: {
+        '/api': {
+          target: 'http://localhost:8000',
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, '')
+        }
       }
     }
   }
 })
-
